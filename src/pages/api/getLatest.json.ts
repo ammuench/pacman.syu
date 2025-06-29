21cf2a89abfde8a2723f14bcd5dffb8f18f6cb9a
Alex Muench <ammuench@gmail.com>
2025-06-28 20:20:22 -0500

    🚧 Add footer

--- src/pages/api/getLatest.json.ts
+++ src/pages/api/getLatest.json.ts
@@ -1,8 +1,37 @@
 import type { APIRoute } from "astro";
 import { db, DbRefresh, ArchUpdate, desc } from "astro:db";
+import Parser from "rss-parser";
+import { v4 as uuidv4 } from "uuid";
+import type { ArchRSSUpdate } from "../../types/ArchRSSUpdate.type";
 
 async function updateRss() {
-  console.log("updateRss stub");
+  const parser = new Parser();
+  const feed = await parser.parseURL("https://archlinux.org/feeds/news/");
+
+  const dbRecords = (feed.items as ArchRSSUpdate[]).map((item) => {
+    const record = {
+      id: uuidv4(),
+      title: item.title,
+      link: item.link,
+      publishDate: new Date(item.pubDate),
+      html: item.content,
+    };
+    return record;
+  });
+
+  try {
+    await db.insert(ArchUpdate).values(dbRecords);
+  } catch (e) {
+    console.error(e);
+  }
+
+  const now = new Date();
+
+  try {
+    await db.insert(DbRefresh).values([{ lastUpdate: now }]);
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 export const GET: APIRoute = async () => {
